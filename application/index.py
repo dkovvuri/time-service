385b9b18cecf4766e9fdc5e85009a3e908c6d91c
Deepak Kovvuri <kovvuri@amazon.com>
2020-07-22 23:05:27 -0400

    Final Changes

--- application/index.py
+++ application/index.py
@@ -1,10 +1,30 @@
 import datetime
 import json
+import pytz
 
 def handler(event,context):
-    print(json.dumps(event,skipkeys=True,indent=4))
+    statusCode = 200
+    path = event['path']
+    if path == "/time":
+        if "timezone" in event['queryStringParameters']:
+            try: 
+                timezone = event['queryStringParameters']['timezone']
+                time = datetime.datetime.now(timezone).strftime("%d/%m/%Y %H:%M:%S")
+                message = "Current Time in {} is {}".format(timezone,time)
+                print(message)
+            except TypeError:
+                statusCode = 400
+                message = "{} is not a valid timezone. Please use /timezones API to fetch a list of valid timezones.".format(timezone)
+        else:
+            time = datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+            message = "Time in UTC is {}".format(time)
+            print(message)
+    elif path == "/timezones":
+        message = json.dumps(pytz.common_timezones,indent=4)
+
     message = {
-        'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!')
+        'statusCode': statusCode,
+        'body': message
     }
+
     return message
